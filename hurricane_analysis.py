4408d9fd21893e052dc169b41837670c191392a4
Steve <87497042+sthermon@users.noreply.github.com>
2022-06-29 22:33:28 -0400

    Update hurricane_analysis.py

--- hurricane_analysis.py
+++ hurricane_analysis.py
@@ -1,4 +1,3 @@
-from collections import OrderedDict
 # names of hurricanes
 names = ['Cuba I', 'San Felipe II Okeechobee', 'Bahamas', 'Cuba II', 'CubaBrownsville', 'Tampico', 'Labor Day', 'New England', 'Carol', 'Janet', 'Carla', 'Hattie', 'Beulah', 'Camille', 'Edith', 'Anita', 'David', 'Allen', 'Gilbert', 'Hugo', 'Andrew', 'Mitch', 'Isabel', 'Ivan', 'Emily', 'Katrina', 'Rita', 'Wilma', 'Dean', 'Felix', 'Matthew', 'Irma', 'Maria', 'Michael']
 
